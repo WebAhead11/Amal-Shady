af0aa0682bdd37017b81c69cf306b3464eb380d4
amalhayek99 <amalhayek10@gmail.com>
2021-09-12 10:53:57 +0300

    added user

--- router.js
+++ router.js
@@ -5,7 +5,12 @@ var router = express.Router();
 const cred = {
   email: "test@test.com",
   password: "test1234",
+  email: "shady@shady.com",
+  password: "shady1234",
+  email: "amal@amal.com",
+  password: "amal1234",
 };
+
 router.post("/login", (req, res) => {
   if (req.body.email == cred.email && req.body.password == cred.password) {
     req.session.user = req.body.email;
