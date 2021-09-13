6198ce7e5cceb4f7029864df4d84223fb462a0be
amalhayek99 <amalhayek10@gmail.com>
2021-09-13 10:02:10 +0300

    old version

--- router.js
+++ router.js
@@ -1,14 +1,29 @@
 // const response = require("express");
 var express = require("express");
 var router = express.Router();
-const cred = {
-  email: "test@test.com",
-  password: "test1234",
-  email: "shady@shady.com",
-  password: "shady1234",
-  email: "amal@amal.com",
-  password: "amal1234",
-};
+
+const fs = require("fs");
+fs.readFile("./user.json", "utf-8", (err,jsonString)=>{
+  if(err) {
+    console.log(err);
+  }else {
+    try{
+    const data = JSON.parse(jsonString);
+    console.log(data.address);
+  }catch(err){
+    console.log('Error parsing JSON, err');
+  }}
+}
+);
+
+// const cred = {
+//   email: "test@test.com",
+//   password: "test1234",
+//   email: "shady@shady.com",
+//   password: "shady1234",
+//   email: "amal@amal.com",
+//   password: "amal1234",
+// };
 
 router.post("/login", (req, res) => {
   if (req.body.email == cred.email && req.body.password == cred.password) {
